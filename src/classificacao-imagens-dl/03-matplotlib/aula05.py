7a520453092aa5eda7b07cef57a40fbea502368c
Gabriel Moutinho <gabegmoutinho@gmail.com>
2026-02-28 21:36:46 -0300

    adicionando atividade 5 sobre subplots

---
+++ src/classificacao-imagens-dl/03-matplotlib/aula05.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 5, 0.1)
+y = x**5
+y1 = x**2
+
+# subplots
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(8, 4))
+ax1.plot(x, y)
+ax2.plot(x, y1)
+ax3.plot(x, y**3)
+ax4.plot(x, y1**4)
+
+plt.show()
