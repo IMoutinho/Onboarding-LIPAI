f097c90e37b8cc8203f08680b0711fe7f85cebd6
Gabriel Moutinho <gabegmoutinho@gmail.com>
2026-02-28 21:55:08 -0300

    aula 5 sobre adicionar vários subplots com um unico axes

--- src/classificacao-imagens-dl/03-matplotlib/aula05.py
+++ src/classificacao-imagens-dl/03-matplotlib/aula05.py
@@ -6,10 +6,40 @@ y = x**5
 y1 = x**2
 
 # subplots
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(8, 4))
-ax1.plot(x, y)
-ax2.plot(x, y1)
-ax3.plot(x, y**3)
-ax4.plot(x, y1**4)
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
+plt.suptitle("Gráficos com Subplots")
+
+plt.subplots_adjust(
+    left=0.152,
+    right=0.943,
+    top=0.9,
+    bottom=0.14,
+    wspace=0.438,
+    hspace=0.4
+)
+
+axes[0, 0].plot(x, y)
+axes[0, 0].set_title("Gráfico x^5")
+axes[0, 0].set_xlabel("Tempo")
+axes[0, 0].set_ylabel("Amplitude")
+axes[0, 0].grid(True)
+
+axes[0, 1].plot(x, y1)
+axes[0, 1].set_title("Gráfico x^2")
+axes[0, 1].set_xlabel("Tempo")
+axes[0, 1].set_ylabel("Amplitude")
+axes[0, 1].grid(True)
+
+axes[1, 0].plot(x, y**3)
+axes[1, 0].set_title("Gráfico x^15")
+axes[1, 0].set_xlabel("Tempo")
+axes[1, 0].set_ylabel("Amplitude")
+axes[1, 0].grid(True)
+
+axes[1, 1].plot(x, y1**4)
+axes[1, 1].set_title("Gráfico x^8")
+axes[1, 1].set_xlabel("Tempo")
+axes[1, 1].set_ylabel("Amplitude")
+axes[1, 1].grid(True)
 
 plt.show()
