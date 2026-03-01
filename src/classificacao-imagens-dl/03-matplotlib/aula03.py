e166b69914cd2b3722920fe47acd433f4541c01d
Gabriel Moutinho <gabegmoutinho@gmail.com>
2026-02-28 21:09:27 -0300

    aula sobre colocar dois graficos em uma mesma figura

--- src/classificacao-imagens-dl/03-matplotlib/aula03.py
+++ src/classificacao-imagens-dl/03-matplotlib/aula03.py
@@ -1,9 +1,21 @@
-#Multiplos graficos em uma mesma figura com matplotlib
+# Multiplos graficos em uma mesma figura com matplotlib
 
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-t = np.arange(0,100)
-plt.figure("Gráfico", figsize=(6,4))
+t = np.linspace(-np.pi, np.pi, 100)
+y = np.cos(t)
+y1 = np.sin(t)
+
+#Tudo que coloco entre plt.figure e plt.show, vai ser plotado na mesma figura, ou seja, no mesmo gráfico
+plt.figure("Gráfico Cosseno - Seno", figsize=(6, 4))
+plt.plot(t, y)
+plt.plot(t, y1)
+plt.title("Gráfico Cosseno - Seno")
+plt.xlabel("Tempo")
+plt.ylabel("Amplitude")
+plt.grid()
+
+plt.show()
